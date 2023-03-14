d11a20cfb4ad7b3e0c712ecbeac13c0d197d767d
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-14 23:46:59 +0530

    [Aniket]- Solved WhileLoop problem

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace WhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to WhileLoop Problems");
+            While.FlipCoin();
+            While.ReverseThestring();
+            While.NaturalNum();
+        }
+    }
+}
---
+++ WhileLoop/While.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    internal class While
+    {
+        public static void FlipCoin()
+        {
+            int headsCount = 0;
+            int tailsCount = 0;
+            Random random = new Random();
+
+            while (headsCount < 20 && tailsCount < 20)
+            {
+                int flip = random.Next(0, 2);
+                if (flip == 0)
+                {
+                    Console.WriteLine("Heads");
+                    headsCount++;
+                }
+                else
+                {
+                    Console.WriteLine("Tails");
+                    tailsCount++;
+                }
+            }
+
+            if (headsCount == 20)
+            {
+                Console.WriteLine("Heads wins!");
+            }
+            else
+            {
+                Console.WriteLine("Tails wins!");
+            }
+        }
+
+        /// <summary>
+        /// 2nd program of while loop
+        /// </summary>
+        public static void ReverseThestring()
+        {
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+            int length = word.Length;
+            string reversedWord = "";
+
+            while (length > 0)
+            {
+                reversedWord += word[length - 1];
+                length--;
+            }
+
+            Console.WriteLine("Reversed word: " + reversedWord);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 3rd program of while loop
+        /// </summary>
+        public static void NaturalNum()
+        {
+            int num = 1;
+            int sum = 0;
+            int count = 0;
+
+            while (count < 5)
+            {
+                sum += num;
+                num++;
+                count++;
+            }
+
+            Console.WriteLine("Sum of first 5 natural numbers: " + sum);
+        }
+    }
+}
