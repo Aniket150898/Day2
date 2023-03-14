0c26acc1e4ab3379e967fbbef1625dfe54e9955e
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-14 21:54:39 +0530

    [Aniket]- Solved Else if statement problem

---
+++ IfElseIf/ElseIf.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct
+{
+    internal class ElseIf
+    {
+        /// <summary>
+        /// else if first program
+        /// </summary>
+
+        public static void LargestNum()
+        {
+            Console.WriteLine("Enter the first number:- ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:- ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number:- ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            // Find the largest number
+            int largest;
+            if (num1 >= num2 && num1 >= num3)
+            {
+                largest = num1;
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                largest = num2;
+            }
+            else
+            {
+                largest = num3;
+            }
+
+            // Output the largest number
+            Console.WriteLine("The largest number is: " + largest);
+        }
+
+        /// <summary>
+        /// else if first program
+        /// </summary>
+        public static void AdmissionEligibility()
+        {
+            Console.WriteLine("Enter the marks obtained in Physics:-");
+            int marks1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the marks obtained in Chemistry:-");
+            int marks2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the marks obtained in Mathematics:-");
+            int marks3 = Convert.ToInt32(Console.ReadLine());
+            if (marks1 >= 55 && marks2 >= 50 && marks3 >= 65)
+            {
+                Console.WriteLine("The candidate is eligible for admission.");
+            }
+            else if (marks1 + marks2 + marks3 >= 180 || marks1 + marks2 + marks3 >= 140)
+            {
+                Console.WriteLine("The candidate is eligible for admission.");
+            }
+            else
+            {
+                Console.WriteLine("The candidate is eligible **Not eligible** for admission.");
+            }
+        }
+    }
+}
---
+++ IfElseIf/Program.cs
@@ -0,0 +1,14 @@
+﻿using ProgrammingConstruct;
+
+namespace IfElseIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //ElseIf.LargestNum();
+            ElseIf.AdmissionEligibility();
+        }
+    }
+}
